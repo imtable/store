0f79c62be6b7dcbe0f93e62a82e061e3e0082328
imtable <mrelbat@gmail.com>
2025-09-29 22:52:05 +0100

    update DB url

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 module.exports = {
-  uri: 'mongodb+srv://admin:admin@imtablefreecluster.asr7n.mongodb.net/store',
+  uri: 'mongodb+srv://imtable:adaD1324@petprojectscluster.khgowa0.mongodb.net/store?retryWrites=true&w=majority&appName=petProjectsCluster',
+  // uri: process.env.MONGODB_URI,
   options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: false,
+    // useFindAndModify: false
   },
 };
